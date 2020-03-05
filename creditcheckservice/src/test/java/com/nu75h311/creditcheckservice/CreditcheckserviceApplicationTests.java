4cc6f7b168f48076e6e2901b79ea9a809bb50ec4
Adriano Tanoue <adriano.katayamatanoue@asml.com>
2020-03-05 16:33:26 +0100

    empty service modules

---
+++ creditcheckservice/src/test/java/com/nu75h311/creditcheckservice/CreditcheckserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nu75h311.creditcheckservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditcheckserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
